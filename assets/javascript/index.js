3301009bb78a8c1895fba41153e8dc9ff23976f2
jackkaroo <mmmekaterina2013@gmail.com>
2020-05-27 12:56:14 +0300

    update

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,5 @@
 
 function getAirports(value){
-   
     fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${value}`, {
         "method": "GET",
         "headers": {
@@ -8,16 +7,21 @@ function getAirports(value){
             "x-rapidapi-key": "cf75ecbd7dmsh76f1f68906a6bd8p1f9fccjsndbf26ec289ab"
         }
     })
-    .then(response => response.json().then((data) => console.log(data)))
+    .then(response => response.json().then((data) => console.log(`Input value is ${value}, response data is `,data)))
     .catch(err => {
         console.log(err);
     });
 
 }
 
-let input = document.getElementsByClassName('content__choose_plane_dest-from')[0];
-input.addEventListener('keypress', function(){
-    getAirports(input.value);
+let airportFrom = document.getElementsByClassName('content__choose_plane_dest-from')[0];
+airportFrom.addEventListener('keypress', function(){
+    getAirports(airportFrom.value);
+});
+
+let airportTo = document.getElementsByClassName('content__choose_plane_dest-to')[0];
+airportTo.addEventListener('keypress', function(){
+    getAirports(airportTo.value);
 });
 
 document.getElementsByClassName('content__transport_btn-plane')[0].addEventListener('click', function(){
