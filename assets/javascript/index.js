1247c38ab462d804632efa3d025045fef1bb5c82
jackkaroo <mmmekaterina2013@gmail.com>
2020-05-27 14:30:26 +0300

    Add datalist

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,28 +1,49 @@
 
-function getAirports(value){
-    fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${value}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-            "x-rapidapi-key": "cf75ecbd7dmsh76f1f68906a6bd8p1f9fccjsndbf26ec289ab"
-        }
-    })
-    .then(response => response.json().then((data) => console.log(`Input value is ${value}, response data is `,data)))
-    .catch(err => {
-        console.log(err);
-    });
+function addListEntry(list, value, text) {
+    var optionNode =  document.createElement("option");
+    optionNode.value = value;
+    optionNode.appendChild(document.createTextNode(text));
+    document.getElementById(list).appendChild(optionNode);
+}
 
+function getAirports(list, value){
+    var xhr = new XMLHttpRequest();
+   
+    xhr.open("GET", `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=${value}`);
+    xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "cf75ecbd7dmsh76f1f68906a6bd8p1f9fccjsndbf26ec289ab");
+
+    // Setup an "event listener".
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 400){
+            var response = JSON.parse(this.responseText);
+
+            Object.values(response)[0].forEach(element => {
+                addListEntry(list, element.PlaceName, "Country: " + element.CountryName);
+            });
+        }
+    };
+    xhr.send();
 }
 
 let airportFrom = document.getElementsByClassName('content__choose_plane_dest-from')[0];
 airportFrom.addEventListener('keypress', function(){
-    getAirports(airportFrom.value);
+    if(airportFrom.value.length>1)
+        getAirports('destination-from', airportFrom.value);
 });
 
 let airportTo = document.getElementsByClassName('content__choose_plane_dest-to')[0];
 airportTo.addEventListener('keypress', function(){
-    getAirports(airportTo.value);
+    if(airportFrom.value.length>1)
+        getAirports('destination-to',airportTo.value);
 });
+
+
+
+
+
+
+
 
 document.getElementsByClassName('content__transport_btn-plane')[0].addEventListener('click', function(){
     document.getElementsByClassName('content__choose')[0].style.display = "flex";
