515d1c232a8641ddee23445500bc81b8e6d69272
jackkaroo <mmmekaterina2013@gmail.com>
2020-06-09 11:43:06 +0300

    update

--- assets/javascript/flights.js
+++ assets/javascript/flights.js
@@ -1,3 +1,6 @@
+const apiKey = 'be557a4761msh859fb1ff66181e9p123d2ajsn9940bf7444e0';
+
+
 const airportFromInput = document.getElementsByClassName('content__choose_plane_dest-from')[0];
 airportFromInput.addEventListener('keypress', function(){
   if(airportFromInput.value.length>2)
@@ -16,7 +19,7 @@ function getAirports(list, value){
   xhr.addEventListener("readystatechange", function () {
     if (this.readyState === this.DONE) {
       let response = JSON.parse(this.responseText);
-      console.log(response);
+      /*console.log(response);*/
       response.forEach(element => {
         addListEntry(list, element)
       })
@@ -25,7 +28,7 @@ function getAirports(list, value){
 
   xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/airports/search?locale=en_US&query=${value}`);
   xhr.setRequestHeader("x-rapidapi-host", "tripadvisor1.p.rapidapi.com");
-  xhr.setRequestHeader("x-rapidapi-key", "cf75ecbd7dmsh76f1f68906a6bd8p1f9fccjsndbf26ec289ab");
+  xhr.setRequestHeader("x-rapidapi-key", apiKey);
 
   xhr.send();
 }
@@ -64,37 +67,18 @@ function openFlightSearchWindow() {
   }
   
   function updateSearchFlightDom(){
-    document.getElementsByClassName('content__choose')[0].style.display = "flex";
+    /*document.getElementsByClassName('content__choose')[0].style.display = "flex";
     document.getElementsByClassName('content__choose_plane')[0].style.display = "none";
     document.getElementsByClassName('content__choose_car')[0].style.display = "none";
     document.getElementsByClassName('content-title')[0].style.display = "none";
     document.getElementsByClassName('content-subtitle')[0].style.display = "none";
-    document.getElementsByClassName('content__transport_btns')[0].style.display="none";
+    document.getElementsByClassName('content__transport_btns')[0].style.display="none";*/
+
+    document.getElementsByClassName('content__response')[0].style.display = "block";
+    document.getElementsByClassName('content__response_flight')[0].style.display = "block";
+
   }
-  
-  function flightCreateSession() {
-    const flightFrom = getInfoAirportFrom();
-    const flightTo = getInfoAirportTo();
-  
-    let xhr = new XMLHttpRequest();
-  
-    xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === this.DONE) {
-        let response = JSON.parse(this.responseText);
-        flightPoll(response);
-        //createFlights(response,flightFrom.placeName,flightTo.placeName)
-      }
-    });
-  
-    //xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/create-session?currency=USD&ta=1&c=0&d1=CNX&o1=DMK&dd1=2020-12-05`);
-    xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/create-session?currency=USD&ta=1&c=0&d1=${flightTo.id}&o1=${flightFrom.id}&dd1=${flightFrom.date}`);
-  
-    xhr.setRequestHeader("x-rapidapi-host", "tripadvisor1.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "cf75ecbd7dmsh76f1f68906a6bd8p1f9fccjsndbf26ec289ab");
-  
-    xhr.send();
-  }
-  
+   
   function getInfoAirportFrom() {
     const airportFrom = {};
     let option = document.getElementsByTagName('option');
@@ -125,6 +109,34 @@ function openFlightSearchWindow() {
     return airportTo;
   }
 
+  /**
+   *  
+   * WORK WITH API STARTS!!!!!!!!!!!!!!!!!
+   * 
+   */
+  function flightCreateSession() {
+    const flightFrom = getInfoAirportFrom();
+    const flightTo = getInfoAirportTo();
+  
+    let xhr = new XMLHttpRequest();
+  
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        let response = JSON.parse(this.responseText);
+        flightPoll(response);
+        //createFlights(response,flightFrom.placeName,flightTo.placeName)
+      }
+    });
+  
+    xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/create-session?dd2=2020-09-15&currency=UAH&ta=1&c=0&d1=VIE&o1=LON&dd1=2020-09-12`);
+    //xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/create-session?dd2=${flightTo.date}&currency=UAH&ta=1&c=0&d1=${flightTo.id}&o1=${flightFrom.id}&dd1=${flightFrom.date}`);
+  
+    xhr.setRequestHeader("x-rapidapi-host", "tripadvisor1.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", apiKey);
+  
+    xhr.send();
+  }
+
   function flightPoll(response){
     console.log(response)
 
@@ -134,19 +146,19 @@ function openFlightSearchWindow() {
         if (this.readyState === this.DONE) {
             let responsePoll = JSON.parse(this.responseText);
             console.log(responsePoll);
-            getBookData(responsePoll);
-            
+            createFlightsOption(responsePoll);
+            //getBookData(responsePoll);
         }
     });
     
-    xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/poll?currency=USD&n=15&ns=NON_STOP%252CONE_STOP&so=PRICE&o=0&sid=${response.search_params.sid}`);
+    xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/poll?currency=UAH&n=15&ns=NON_STOP%252CONE_STOP&so=PRICE&o=0&sid=${response.search_params.sid}`);
     xhr.setRequestHeader("x-rapidapi-host", "tripadvisor1.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "cf75ecbd7dmsh76f1f68906a6bd8p1f9fccjsndbf26ec289ab");
+    xhr.setRequestHeader("x-rapidapi-key", apiKey);
     
     xhr.send();
   }
 
-  function getBookData(responsePoll){
+  function getBookData(responsePoll, responseId){
     
     var xhr = new XMLHttpRequest();
 
@@ -157,23 +169,130 @@ function openFlightSearchWindow() {
       }
     });
 
-    xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/get-booking-url?searchHash=${responsePoll.summary.sh}&Dest=VIE&id=${responsePoll.itineraries[0].l[0].id}&Orig=KBP&searchId=${responsePoll.search_params.sid}`);
+    xhr.open("GET", `https://tripadvisor1.p.rapidapi.com/flights/get-booking-url?searchHash=${responsePoll.summary.sh}&Dest=VIE&id=${responseId}&Orig=KBP&searchId=${responsePoll.search_params.sid}`);
     xhr.setRequestHeader("x-rapidapi-host", "tripadvisor1.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "cf75ecbd7dmsh76f1f68906a6bd8p1f9fccjsndbf26ec289ab");
+    xhr.setRequestHeader("x-rapidapi-key", apiKey);
 
     xhr.send();
+    return 1;
   }
 
   
-  function createFlights(response,placeNameFrom,placeNameTo){
+  function createFlightsOption(response) {
+    let itineraries = response.itineraries;
+    if(!itineraries) return 0;
+    let airportMap = getResponseFlightAirportMap(response);
+    let planeMap = getResponseFlightPlaneMap(response);
+    let carriersMap = getResponseFlightCarriersMap(response);
+
+    for(let i=0;i<itineraries.length;i++){
+      
+      let obj = itineraries[i];
+      
+      let originPlace = obj.f[0].l[0].da; //id but we can use abbr
+      let destinationPlace = obj.f[0].l[0].aa;
+      let originDate = obj.f[0].l[0].dd; //split on day and time
+      let destinationDate = obj.f[0].l[0].ad;
+
+      let carrierPlaneId = obj.f[0].l[0].e; //can be empty
+
+      let carrierAirCompanyId = obj.f[0].l[0].m; //find by id name and image
+      let carrierCompany = obj.l[0].s; //always cheap one
+
+
+
+      let price = obj.l[0].pr.p; //in usd
+      
+
+      let optionWrapper = document.createElement('div');
+      optionWrapper.classList.add('content__response_flight_item')
+      document.getElementsByClassName('content__response_flight_items')[0].appendChild(optionWrapper)
+
+      let optionTicket = document.createElement('div');
+      optionTicket.classList.add('content__response_flight_item_ticket')
+      optionTicket.insertAdjacentHTML('beforeend', 
+      `<div class="content__response_flight_item_ticket--wrapper">
+        <div class="content__response_flight_item_ticket--date">${getResponseFlightDate(originDate)}
+        </div>
+        <div class="content__response_flight_item_ticket--flight">
+          <div class="content__response_flight_item_ticket--flight-time">${getResponseFlightTime(originDate)}</div>
+          <div class="content__response_flight_item_ticket--flight-airport">${airportMap.get(originPlace)[0]} <abbr title="${airportMap.get(originPlace)[1]}">(${originPlace})</abbr> </div>
+        </div>
+        <div class="content__response_flight_item_ticket--flight content__response_flight_item_ticket--flight-1">
+          <div class="content__response_flight_item_ticket--flight-time">${getResponseFlightTime(destinationDate)}</div>
+          <div class="content__response_flight_item_ticket--flight-airport">${airportMap.get(destinationPlace)[0]} <abbr title="${airportMap.get(destinationPlace)[1]}">(${destinationPlace})</abbr></div>
+        </div>
+        <div class="content__response_flight_item_ticket--carrier">${carriersMap.get(carrierAirCompanyId)[0]}
+        </div>
+        <div class="content__response_flight_item_ticket--price"> <span>from</span><br> ${price} <span>uah</span></div>
+      </div>`);
+      optionWrapper.appendChild(optionTicket);
+
+      /**<!--<div class="content__response_flight_item_ticket--carrier-plane">${carriersMap.get(carrierPlaneId)[0]}</div>
+          <div class="content__response_flight_item_ticket--carrier-company">${carrierCompany}</div>--> */
+
+
+      let optionButton = document.createElement('div');
+      optionButton.classList.add('content__response_flight_item_button')
+      // getBookData(response).then(console.log(link));
+      optionButton.addEventListener('click', function () {
+        getBookData(response, obj.l[0].id);
+      })
+      optionButton.insertAdjacentHTML('beforeend', `<button class="button">BUY</button>`);
+      optionWrapper.appendChild(optionButton);
+
+    }
+  }
+
+  function getResponseFlightDate(baseDate) {
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const dayNames = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat",
+    "Sun"];
+
+    let date = new Date(baseDate)
+    let newDate = `${dayNames[date.getDay()]}, ${date.getDate()} ${monthNames[date.getMonth()]} ${date.getFullYear()}`
+    
+    return newDate;
+  }
+
+  function getResponseFlightTime(baseDate) {
+    let date = new Date(baseDate)
+
+    return date.toLocaleTimeString().substring(0,5)
+  }
+
+  function getResponseFlightAirportMap(response) { 
+    let map = new Map();
+    response.airports.forEach(element => {
+      map.set(element.c,[element.cn,element.n])
+    })
+    return map;
+  }
+
+  function getResponseFlightCarriersMap(response) { 
+    let map = new Map();
+    response.carriers.forEach(element => {
+      map.set(element.c,[element.n,element.l])
+    })
+    return map;
+  }
+
+  function getResponseFlightPlaneMap(response) { 
+    let map = new Map();
+    response.equipment.forEach(element => {
+      map.set(element.i,element.n)
+    })
+    return map;
+  }
+
+
+
+
+  /*function createFlightsd(response,placeNameFrom,placeNameTo){
     console.log(response)
     let mapCarriers = getCarriersMapId(response);
     let mapPlaces = getPlacesMapId(response);
-  
-    let menuBtn = document.createElement('button');
-    menuBtn.classList.add('content__response-menubtn')
-    menuBtn.innerHTML = 'Menu'
-    document.getElementsByClassName('content__wrapper')[0].appendChild(menuBtn)
    
     let responseWrapper =  document.createElement('div');
     responseWrapper.classList.add('content__response')
@@ -220,4 +339,4 @@ function openFlightSearchWindow() {
     let newDate= `${date}.${month}.${year}`
   
     return newDate;
-  }
+  }*/
