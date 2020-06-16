60a19c2e418f6bd9906f102792206fb6200e29cc
jackkaroo <mmmekaterina2013@gmail.com>
2020-06-16 16:03:40 +0300

    fix bugs with wrappers

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,14 @@
 /**ADDING EVENT LISTENERS ON BUTTONS PLANE AND CAR*/
 function openFlightChooseWindow() {
   document.getElementsByClassName('content__choose_car')[0].style.display = "none";
-  
+  document.getElementsByClassName('content__response')[0].style.display = "none";
+
+
+  if(document.getElementsByClassName('content__choose_plane-wrapper')[0]) {
+    document.getElementsByClassName('content__choose')[0].appendChild(document.getElementsByClassName('content__choose_plane')[0]);
+    document.getElementsByClassName('content__choose')[0].removeChild(document.getElementsByClassName('content__choose_plane-wrapper')[0]);
+  }
+
   let planeWrapper = document.createElement('div');
   planeWrapper.classList.add('content__choose_plane-wrapper');
   planeWrapper.setAttribute('data-aos','fade-up');
@@ -9,11 +16,18 @@ function openFlightChooseWindow() {
   document.getElementsByClassName('content__choose')[0].appendChild(planeWrapper);
   planeWrapper.appendChild(document.getElementsByClassName('content__choose_plane')[0])
   document.getElementsByClassName('content__choose_plane')[0].style.display = "flex";
+ 
 }
 
 function openCarChooseWindow() {
   document.getElementsByClassName('content__choose_plane')[0].style.display = "none";
   document.getElementsByClassName('content__response')[0].style.display = "none";
+
+  
+  if(document.getElementsByClassName('content__choose_car-wrapper')[0]) {
+    document.getElementsByClassName('content__choose')[0].appendChild(document.getElementsByClassName('content__choose_car')[0]);
+    document.getElementsByClassName('content__choose')[0].removeChild(document.getElementsByClassName('content__choose_car-wrapper')[0]);
+  }
 
   let carWrapper = document.createElement('div');
   carWrapper.classList.add('content__choose_car-wrapper');
